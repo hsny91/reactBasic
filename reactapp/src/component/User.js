df1bc1f409c42892588ba0f9b03b8102838e7a26
Husniye <husniyesakmak.91@gmail.com>
2020-12-28 15:08:34 +0100

    add bootstrap

--- reactapp/src/component/User.js
+++ reactapp/src/component/User.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react'
 
  class User extends Component {
     render() {
-        const {name,salary}=this.props;  //destructing
+        const {name,salary,department}=this.props;  //destructing
         return (
-            <div>
-                <ul>
-                    <li>adi:{name}</li>
-                    <li>salary:{salary}</li>
-                </ul>
+            <div className="col-md-8 mb-4">
+                <div className="card">
+                    <div className="card-header d-flex            justify-content-between">
+                        <h4 className=" d-inline">{name}</h4>
+                        <i className="fas fa-trash-alt" style={{cursor:"pointer"}}></i>
+                    </div>
+                    <div className="card-body">
+                        <p className="card-text"> Maas:{salary}</p>
+                        <p className="card-text"> Deparment:{department}</p>
+                    </div>
+                </div>
+               
+                  
+                
             </div>
         )
     }
