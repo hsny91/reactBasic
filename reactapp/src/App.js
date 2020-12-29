91c5025f299d5f7749f78792781d4cc1c9627f43
Husniye <husniyesakmak.91@gmail.com>
2020-12-29 20:58:28 +0100

    context api added to project

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -42,3 +42,41 @@ class App extends Component {
 }
 
 export default App;
+
+/**
+ * props drilling
+ * import React, { Component } from 'react'
+
+ class User extends Component {
+     onDeleteUser=(e)=>{
+        const{id,deleteUser}=this.props
+        deleteUser(id)
+     }
+
+    render() {
+        const {id,name,salary,department}=this.props;  //destructing
+        console.log("calsi")
+        return (
+            <div className="col-md-8 mb-4">
+                <div className="card">
+                    <div className="card-header d-flex            justify-content-between">
+                        <h4 className=" d-inline">{name}</h4>
+                        <i onClick={this.onDeleteUser} className="fas fa-trash-alt" style={{cursor:"pointer"}}></i>
+                    </div>
+                    <div className="card-body">
+                    <p className="card-text"> id:{id}</p>
+                    <p className="card-text"> Name:{name}</p>
+                        <p className="card-text"> Maas:{salary}</p>
+                        <p className="card-text"> Deparment:{department}</p>
+                    </div>
+                </div>
+               
+                  
+                
+            </div>
+        )
+    }
+}
+
+export default User;
+ */
--- reactapp/src/component/Users.js
+++ reactapp/src/component/Users.js
@@ -26,3 +26,36 @@ import User from './User'
     }
 }
  export default Users;
+
+
+ /**
+  * props drilling
+  * import React, { Component } from 'react'
+import User from './User'
+
+ class Users extends Component {
+    render() {
+        const {users,deleteUser}=this.props;
+        console.log("deneme")
+        return (
+            <div>
+                {
+                users.map(user=>{
+                    return(
+                        <User
+                        key={user.id}
+                        id={user.id}
+                        name={user.name}
+                        salary={user.salary}
+                        department={user.department}
+                        deleteUser={deleteUser}
+                        />
+                    )
+                })     
+                }
+      </div>
+        )
+    }
+}
+ export default Users;
+  */
