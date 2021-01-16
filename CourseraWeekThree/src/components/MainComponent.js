80da87ecad55ce6791b71ba2c9b6aa7be800d426
Husniye <husniyesakmak.91@gmail.com>
2021-01-16 20:51:42 +0100

    create react-redux-form

--- CourseraWeekThree/src/components/MainComponent.js
+++ CourseraWeekThree/src/components/MainComponent.js
@@ -43,20 +43,19 @@ constructor(props){
     
     return (
       <div>
-        <Header />
-        <div>
-          <Switch>
+       <Header />
+       <Switch>
               <Route path='/home' component={HomePage} />
-              <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders} />} />} />
               <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
               <Route path='/menu/:dishId' component={DishWithId} />
-              <Route exact path='/contactus' component={Contact} />} />
+              <Route exact path='/contactus' component={Contact}  />
+              <Route path='/aboutus'component={() => <About leaders={this.props.leaders} />} />
               <Redirect to="/home" />
           </Switch>
-        </div>
         <Footer />
       </div>
     );
   }
 }
+
 export default withRouter(connect(mapStateToProps)(Main));
